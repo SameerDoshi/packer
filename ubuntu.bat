33d99750a6f0fe1b663c185006d3ce9d470a340d
SameerDoshi <sameer.doshi@gmail.com>
2019-03-19 16:40:19 -0500

    first commit

---
+++ ubuntu.bat
@@ -0,0 +1,6 @@
+packer build ^
+    -var "client_id=xxxx" ^
+    -var "client_secret=xxxxxx" ^
+    -var "tenant_id=xxxxx" ^
+    -var "subscription_id=xxxxx" ^
+    ubuntu_v3.json
---
+++ windows.bat
@@ -0,0 +1,6 @@
+packer build ^
+    -var "client_id=xxx" ^
+    -var "client_secret=xx" ^
+    -var "tenant_id=xx" ^
+    -var "subscription_id=xxx" ^
+    windows_v2.json
